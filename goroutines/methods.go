ff08608d34d4b795dc95a7ac1bb1fed60bf8559f
Erickype <erickype@hotmail.com>
2022-10-01 16:56:59 -0500

    Wait group param added and done() function

--- goroutines/methods.go
+++ goroutines/methods.go
@@ -3,13 +3,16 @@ package goroutines
 import (
 	"fmt"
 	"github.com/google/uuid"
+	"sync"
 )
 
-func GenerateIDs(IDsChan chan string) {
+func GenerateIDs(IDsChan chan string, wg *sync.WaitGroup) {
 	id := uuid.New()
 	IDsChan <- id.String()
+	wg.Done()
 }
-func LogIds(IDsChan chan string) {
+func LogIds(IDsChan chan string, wg *sync.WaitGroup) {
 	id := <-IDsChan
 	fmt.Printf("Id: %v\n", id)
+	wg.Done()
 }
