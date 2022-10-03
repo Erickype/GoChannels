8c1c3332ac8c4db0704a89c1209a0a920fa0f5ca
Erickype <erickype@hotmail.com>
2022-10-03 14:50:14 -0500

    Call of goroutine GenerateFakeIds function

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 
 func main() {
 	IDsChan := make(chan string)
+	fakeIDsChan := make(chan string)
 	wg := &sync.WaitGroup{}
 
-	wg.Add(2)
+	wg.Add(3)
 
 	go goroutines.GenerateIDs(IDsChan, wg)
-	go goroutines.LogIds(IDsChan, wg)
+	go goroutines.GenerateFakeIDs(fakeIDsChan, wg)
+	go goroutines.LogIds(IDsChan, fakeIDsChan, wg)
 
 	wg.Wait()
 }
